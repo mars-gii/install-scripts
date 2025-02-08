c5a619fe130192615b0e1affa376ab890e7a2db0
mars-gii <mars_gii@hotmail.com>
2025-02-07 16:46:57 -0800

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S thunar neovim nitrogen volumeicon ripgrep htop flameshot dunst
+sudo pacman -S thunar neovim nitrogen volumeicon ripgrep htop flameshot dunst less sxhkd 
